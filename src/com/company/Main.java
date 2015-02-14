4cc5ce7b889fd07b05316bbff256f071c4f20513
Edmundo <jmundo94@gmail.com>
2015-02-14 00:37:05 -0600

    Primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+
+
+        //Creamos el View
+        Pantalla miPantlla = new Pantalla();
+
+
+        // Se llama al metodo iniciarPantalla.
+        miPantlla.iniciarPantalla();
+
+	// write your code here
+    }
+}
